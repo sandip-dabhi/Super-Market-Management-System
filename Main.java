65dd2af7957abc90034afb6b4d1e1e53f3f15cc7
Sandip Dabhi <sandipdabhi970@gmail.com>
2025-05-27 09:42:56 +0530

    Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,5 @@
-
-
 public class Main {
     public static void main(String[] args){
         new Dashboard();
-//         new Customer();
-//        new Admin();
-//        new Stock();
-//        new Login("c_login");
-//        new Signin("a_login");
-//        new test();
-//            new Bill();
-//        showTable.generateTable("Products",5);
     }
 }
